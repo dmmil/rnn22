16d9a59f197466262316eba6e68575578930dbb1
dmmil <dmmil>
2022-12-05 11:53:03 +0300

    дале

---
+++ tests.py
@@ -0,0 +1,33 @@
+from main_python import Main
+
+
+def test_answer():
+    app = Main('Predict',
+               'example_data_forecasting/settings_common.ini',
+               'example_data_forecasting/settings_rnn1.ini',
+               'example_data_forecasting/settings_rnn2.ini')
+
+    # init rnn1 processing
+    app.rnn1.start_process_signals()
+
+    # process rnn1
+    ok = True
+    while ok:
+        ok = app.rnn1.process_signals()
+
+        if app.rnn1.cntr == 35 * 4 or app.rnn1.cntr == 40 * 4 \
+                or app.rnn1.cntr == 45 * 4:
+            # copy model
+            app.rnn2.paste_model(app.rnn1_to_rnn2(app.rnn1.copy_model()))
+
+            # process rnn2
+            ok2 = True
+            while ok2:
+                ok2 = app.rnn2.process_signals()
+            app.rnn2.finish_process_signals()
+
+            # info about rnn2 finished, then can continue
+            app.rnn1.rnn2finished()
+    app.rnn1.finish_process_signals()
+
+    assert True
